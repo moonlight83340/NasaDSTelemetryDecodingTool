9284a2a5303bf429ce2a37532edacff580ca78e9
moonlight83340 <moonlight83340@users.noreply.github.com>
2023-12-18 15:34:14 +0900

    Add commentary

--- DSHKTelemetryDecoder.h
+++ DSHKTelemetryDecoder.h
@@ -28,8 +28,22 @@ public:
 	  \return 0 if everything fine 1 if error
 	*/
 	int processBinaryFile();
+	//! Get the packets extrated from the binary file
+	/*!
+	  \fn std::vector<DS_HkPacket_t> getPackets()
+	  \return vector of DS_HkPacket_t readed from the binary file
+	*/
 	std::vector<DS_HkPacket_t> getPackets();
+	//! Show values of the packet in param
+	/*!
+	  \fn void showPacket(DS_HkPacket_t packet)
+	  \param packet a packet to show values
+	*/
 	void showPacket(DS_HkPacket_t packet);
+	//! Show values of all the packets reacded fron the binary file
+	/*!
+	  \fn void showPackets()
+	*/
 	void showPackets();
 private:
 	//Utility function could be from an another file
--- Utils.h
+++ Utils.h
@@ -2,14 +2,22 @@
 #define UTILS_H
 
 #include <cstdint>
-#include <cstring> // pour memcpy
+#include <cstring> // for memcpy
 
-//Convert uint16_t little endian to big endian
+//! Convert uint16_t little endian to big endian
+/*!
+    \fn inline uint16_t littleToBigEndian16(uint16_t value)
+    \return the uint16_t value in big endian
+*/
 inline uint16_t littleToBigEndian16(uint16_t value) {
     return (value << 8) | (value >> 8);
 }
 
-//Convert uint32_t little endian to big endian
+//! Convert uint32_t little endian to big endian
+/*!
+    \fn inline uint32_t littleToBigEndian32(uint32_t value)
+    \return the uint32_t value in big endian
+*/
 inline uint32_t littleToBigEndian32(uint32_t value) {
     return ((value << 24) & 0xFF000000) |
         ((value << 8) & 0x00FF0000) |
@@ -17,7 +25,11 @@ inline uint32_t littleToBigEndian32(uint32_t value) {
         ((value >> 24) & 0x000000FF);
 }
 
-//Check if CPU little endian
+//! Check if CPU little endian
+/*!
+    \fn bool isLittleEndian()
+    \return true if cpu is little endian flase if it's big endian
+*/
 inline bool isLittleEndian() {
     uint32_t testValue = 0x12345678;
     uint8_t* bytePtr = reinterpret_cast<uint8_t*>(&testValue);
